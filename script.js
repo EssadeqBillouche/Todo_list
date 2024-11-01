62f8ef4234579c0866af5f706bb4e03f4a1d5e5d
MrNobodyCode <essadeq.billouche@gmail.com>
2024-11-01 15:40:10 +0100

    finally add add task function worked  #3

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-let add_button = document.getElementById("add_button");
-let todo_card_body = document.getElementById("task_form");
+let add_button_header = document.getElementById("add_button_header"); // button for the header add  
+const hidden_form = document.getElementById("task_form");
 
+let add_task_form = document.getElementById("add_task_form"); // button for form add
+const todo_card = document.getElementById("todo_card_body"); 
 
-add_button.onclick = function(){
-    todo_card_body.style.display = 'block';
+add_button_header.onclick = function() { // function to show the pop-up form
+    hidden_form.style.display = 'block';
 }
+
+
+add_task_form.addEventListener("click",(e)=>{
+    e.preventDefault()
+    todo_card.innerHTML = `<div class="col-md-4 w-100"> 
+                                <div class="card text-bg-primary mb-3">
+                                    <div class="card-header">TEST TASK</div>
+                                    <div class="card-body">
+                                        <p>HELLO this just a test if it will work correctly without any prob</p>
+                                    </div>
+                                    <div class="card-footer">
+                                        <button type="button" class="btn btn-secondary" id="addmanyButton">EDIT</button>
+                                        <button type="button" class="btn btn-primary" id="addmanyButton">DELETE</button>
+                                    </div>
+                                </div>
+                            </div>`
+    hidden_form.style.display = 'none';
+})
