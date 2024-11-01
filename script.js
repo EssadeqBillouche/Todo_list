f0901b7a31f20b43f5df4a1f8f1c8c8fb738101b
MrNobodyCode <essadeq.billouche@gmail.com>
2024-11-01 12:17:44 +0100

    add task pop with js

---
+++ script.js
@@ -0,0 +1,7 @@
+let add_button = document.getElementById("add_button");
+let todo_card_body = document.getElementById("task_form");
+
+
+add_button.onclick = function(){
+    todo_card_body.style.display = 'block';
+}
