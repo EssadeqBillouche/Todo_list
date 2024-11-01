59ec8c4f04e6f7cd8709d5a705ae5fa2353468a4
MrNobodyCode <essadeq.billouche@gmail.com>
2024-11-01 20:41:24 +0100

    finaly fix the radio box get value prob and understand the logic behind it

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 let add_button_header = document.getElementById("add_button_header"); // button for the header add  
 const hidden_form = document.getElementById("task_form");
-
 let add_task_form = document.getElementById("add_task_form"); // button for form add
-const todo_card = document.getElementById("todo_card_body"); 
+const todo_card = document.getElementById("todo_card_body");
 
-add_button_header.onclick = function() { // function to show the pop-up form
+let task_name = document.getElementById("");
+
+
+
+
+add_button_header.onclick = function () { // function to show the pop-up form
     hidden_form.style.display = 'block';
 }
 
 
-add_task_form.addEventListener("click",(e)=>{
+add_task_form.addEventListener("click", (e) => {
     e.preventDefault()
-    todo_card.innerHTML = `<div class="col-md-4 w-100"> 
+    const prioritys = document.querySelectorAll('input[name="gridRadios"]:checked');
+    let priority = prioritys[0].value;
+    
+    console.log(priority.value);
+
+
+    todo_card.innerHTML += `<div class="col-md-4 w-100"> 
                                 <div class="card text-bg-primary mb-3">
-                                    <div class="card-header">TEST TASK</div>
+                                    <div class="card-header">${priority}</div>
                                     <div class="card-body">
                                         <p>HELLO this just a test if it will work correctly without any prob</p>
                                     </div>
@@ -24,4 +34,5 @@ add_task_form.addEventListener("click",(e)=>{
                                 </div>
                             </div>`
     hidden_form.style.display = 'none';
+
 })
